af7740dc2d88a1c4208f7f8ff1b740c461abde95
carlosmencos-crypto <carlos.mencos@gmail.com>
2026-09-14 15:38:53 -0600

    refactor(v70): remove obsolete map overlay bridge

--- src/components/V70MapParityBridge.tsx
+++
@@ -1,5 +0,0 @@
-import { V70OperationalMap } from "./V70OperationalMap";
-
-export function V70MapParityBridge() {
-  return <div className="v70-route-overlay" data-v70-map-surface="true"><V70OperationalMap /></div>;
-}
